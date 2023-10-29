5be729a35d4ea6e3be4892cc7e04dfb26a61c368
RoxanaEmiliaAdam <123458672+RoxanaEmiliaAdam@users.noreply.github.com>
2023-10-29 13:59:55 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Login } from "./components.jsx/Login";
+import { Register } from "./components.jsx/Register";
+import "./index.css";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
+  return (
+    <div className="App">
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Register onFormSwitch={toggleForm} />
+      )}
+    </div>
+  );
+}
+
+export default App;
